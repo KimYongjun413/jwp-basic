17c0a266ccb0d64da3cd9b8f269ac37531105d7f
kimyongjun413 <kimyongjun413@gmail.com>
2019-05-06 23:44:07 +0900

    UpdateUserFormServlet,UserSessionUtils 개발

---
+++ src/main/java/next/web/UpdateUserFormServlet.java
@@ -0,0 +1,30 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class UpdateUserFormServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(CreateUserServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = req.getParameter("userId");
+        User user = DataBase.findUserById(userId);
+        if(!UserSessionUtils.isSameUser(req.getSession(), user)) {
+            throw new IllegalStateException("다른 사용자의 정볼르 수정할 수 없습니다.");
+        }
+        req.setAttribute("user", user);
+        RequestDispatcher rd = req.getRequestDispatcher("/user/updateForm.jsp");
+        rd.forward(req,resp);
+    }
+}
